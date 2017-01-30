617d80269dbd2c1bc5c5dfd987fbfd5fb0422abe
Munadil Fahad <munadil98@gmail.com>
2017-01-30 11:00:15 +0600

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,22 @@
+$(document).ready(function() {
+
+	$(".textclass").keyup(function(){
+
+		var	charnumber=$(".textclass").val().length;
+		var remainingchar=60-charnumber;
+		
+		if(remainingchar <=-1){
+
+			$(".limit").text(remainingchar).addClass("red");
+		}
+		else{
+			$(".limit").text(remainingchar).removeClass("red");
+		}
+
+
+	});	
+
+
+});
+
 
